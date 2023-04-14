bf006ef42801f095f570b4658f8f4e580879c028
Dika1485 <ptiandika@gmail.com>
2023-04-14 22:42:46 +0700

    tes midtrans

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,45 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class WebController extends Controller
 {
-    public function index(Request $request){
-        /*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
-        composer require midtrans/midtrans-php
-                              
-        Alternatively, if you are not using **Composer**, you can download midtrans-php library 
-        (https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
-        the file manually.   
-
-        require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
-
-        //SAMPLE REQUEST START HERE
-
-        // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
-
-        $params = array(
-            'transaction_details' => array(
-                'order_id' => rand(),
-                'gross_amount' => 10000,
-            ),
-                'customer_details' => array(
-                'first_name' => 'budi',
-                'last_name' => 'pratama',
-                'email' => 'budi.pra@example.com',
-                'phone' => '08111222333',
-            ),
-        );
-
-        $snapToken = \Midtrans\Snap::getSnapToken($params);
-    
-        return view('welcome');
+	public function index(Request $request){
+		return view('welcome');
+	}
+    public function payment(Request $request){
+    	/*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+		composer require midtrans/midtrans-php  
+		                            
+		Alternatively, if you are not using **Composer**, you can download midtrans-php library 
+		(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
+		the file manually.   
+		
+		require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+		
+		//SAMPLE REQUEST START HERE
+		
+		// Set your Merchant Server Key
+		\Midtrans\Config::$serverKey = 'SB-Mid-server-RzyhHLrbAEtlPuE9v66YnQUK';
+		// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+		\Midtrans\Config::$isProduction = false;
+		// Set sanitization on (default)
+		\Midtrans\Config::$isSanitized = true;
+		// Set 3DS transaction for credit card to true
+		\Midtrans\Config::$is3ds = true;
+		
+		$params = array(
+		    'transaction_details' => array(
+		        'order_id' => rand(),
+		        'gross_amount' => 13000,
+		    ),
+		    "item_details" => array( 
+		        [
+		        	'id'=> 'a01',
+		        	'price'=> 7000,
+		        	'quantity'=> 1,
+		        	'name'=> 'Apple'
+		        ],
+		        [
+		        	'id'=> 'b02',
+		        	'price'=> 3000,
+		        	'quantity'=> 2,
+		        	'name'=> 'Orange'
+		        ]
+		    ),
+		    'customer_details' => array(
+		        'first_name' => $request->get('name'),
+		        'last_name' => '',
+		        'email' => $request->get('email'),
+		        'phone' => $request->get('nohp'),
+		    ),
+		);
+		
+		$snapToken = \Midtrans\Snap::getSnapToken($params);
+    	return view('payment',['snapToken'=>$snapToken]);
+    }
+    public function payment_post(Request $request){
+    	$json=json_decode($request->get('json'));
+    	$order=new Order();
+    	$order->status=$json->transaction_status;
+    	$order->transaction_id=$json->transaction_id;
+    	$order->order_id=$json->order_id;
+    	$order->gross_amount=$json->gross_amount;
+    	$order->payment_type=$json->payment_type;
+    	$order->payment_code=isset($json->payment_code)?$json->payment_code:null;
+    	$order->pdf_url=isset($json->pdf_url)?$json->pdf_url:null;
+    	$order->name=$request->get('name');
+    	$order->email=$request->get('email');
+    	$order->nohp=$request->get('nohp');
+    	return $order->save()?redirect(url('/'))->with('alert-success','Order berhasil dibuat'):redirect(url('/'))->with('alert-failed','Terjadi kesalahan');
     }
 }
